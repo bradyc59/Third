ba5b16b980d4d989598f08a4bb8c6a4d2c16a7ba
Conor Brady <conor.brady59@mail.dcu.ie>
2022-02-28 11:19:45 +0000

    done

--- Simmonopoly/urls.py
+++ Simmonopoly/urls.py
@@ -1,14 +1,23 @@
-from django.contrib.auth.decorators import login_required
-from django.urls import path
-from . import views
+"""Simmonopoly URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import include
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name="index"), # /app
-    path('game/<host_game>', views.GameView.as_view(), name="game"),
-    path('', views.LoginView.as_view(), name="index"), # /app
-    path('game/<host_name>', views.GameView.as_view(), name="game"),
-    path('profile/<profile_user>', login_required(views.ProfileView.as_view()), name="profile"),
-    path('join/<user_name>', login_required(views.JoinView.as_view()), name="join"),
-    path('register/', views.CaUserSignupView.as_view(), name='confirm'),
-
+    url('', include('monopoly.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
