14be6a893fda86d3fc294670cf74198d9d459dea
Conor Brady <conor.brady59@mail.dcu.ie>
2022-03-03 12:47:19 +0000

    Auction

--- monopoly/urls.py
+++ monopoly/urls.py
@@ -9,7 +9,7 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name="login"),  # /app
     path('', views.index, name="index"),
-    path('play/<room_code>', game),
+    path('logout/', views.logout_view, name="logout"),
     path('profile/<profile_user>', login_required(views.ProfileView.as_view()), name="profile"),
     path('register/', views.CaUserSignupView.as_view(), name='confirm'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
